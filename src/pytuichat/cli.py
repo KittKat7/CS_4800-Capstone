8233a138900f11f1d21d2ac53c01628da80a9806
Daniel Kindy <kindyda@appstate.edu>
2026-03-14 22:41:53 -0400

    Add support for file creation to CLI Some of them are a bit long winded compared to yours, I apologize. I wanted them to have premade demo messages rather than us having to do crazy things like type out a perfectly formatted json string mid demonstration.

--- src/pytuichat/cli.py
+++ src/pytuichat/cli.py
@@ -1,6 +1,7 @@
 import sys
 
 from inbox import *
+from filereader import *
 
 args = sys.argv[1:]
 
@@ -22,3 +23,115 @@ match args[0]:
         inbox: Inbox = Inbox()
         isOnline: bool = inbox.ping(Contact(args[1]))
         print("User " + args[1] + " is online: " + str(isOnline))
+
+    case "printdirs":
+        _home = os.path.expanduser("~")
+        xdg_data_home = os.environ.get("XDG_DATA_HOME") or \
+            os.path.join(_home, ".local", "share")
+        xdg_config_home = os.environ.get("XDG_CONFIG_HOME") or \
+            os.path.join(_home, ".config", "pytui")
+        print("Data:", xdg_data_home, "Config:", xdg_config_home)
+
+    case "store":
+        # Currently only stores premade messages for demonstration purposes
+        con = Contact('bob1235')
+        con2 = Contact('jerrythesnail')
+        con._displayname = 'bob'
+        con._isBlocked = False
+        con2._isBlocked = False
+        con._status = ContactStatus(2)
+        con2._status = ContactStatus(1)
+        match args[1]:
+            case "1":
+                mes = Message("hi", con)
+                mes._status = MessageStatus(5)
+                mes._sent = datetime.now()
+                mes._recieved = datetime.now()
+                dm = DeliveryMessage(mes, [], [con, con2])
+                FileReader.storeMessage(dm)
+            case "2":
+                mes = Message("okay then", con)
+                mes._status = MessageStatus(2)
+                mes._sent = datetime.now()
+                mes._recieved = datetime.now()
+                dm = DeliveryMessage(mes, [con], [con, con2])
+                FileReader.storeMessage(mes)
+            case "3":
+                mes = Message("bye", con)
+                mes._status = MessageStatus(1)
+                mes._sent = datetime.now()
+                mes._recieved = datetime.now()
+                dm = DeliveryMessage(mes, [con, con2], [con, con2])
+                FileReader.storeMessage(mes)
+    
+    case "clear":
+        # First demo message above will be cleared here
+        FileReader.clearSent()
+
+    case "makesettings":
+        FileReader.makeSettings()
+
+    case "getsetting":
+        FileReader.getSetting(args[1])
+
+    case "changesetting":
+        FileReader.changeSetting(args[1], args[2])
+
+    case "update":
+        # Same demonstration messages as unsent storage
+        con = Contact('bob1235')
+        con2 = Contact('jerrythesnail')
+        con._displayname = 'bob'
+        con._isBlocked = False
+        con2._isBlocked = False
+        con._status = ContactStatus(2)
+        con2._status = ContactStatus(1)
+        ch = Chat([con, con2])
+        his = []
+        unr = 0
+        if len(args) > 1 and args[1] == "1":
+            mes1 = Message('hi', con)
+            mes1._status = MessageStatus(5)
+            mes1._sent = datetime.now()
+            mes1._recieved = datetime.now()
+            his.append(mes1)
+            unr += 1
+        if len(args) > 2 and args[2] == "1":
+            mes2 = Message('okay then', con)
+            mes2._status = MessageStatus(2)
+            mes2._sent = datetime.now()
+            mes2._recieved = datetime.now()
+            his.append(mes2)
+            unr += 1
+        if len(args) > 3 and args[3] == "1":
+            mes3 = Message('goodbye', con)
+            mes3._status = MessageStatus(1)
+            mes3._sent = datetime.now()
+            mes3._recieved = datetime.now()
+            his.append(mes3)
+            unr += 1
+        ch._history = his
+        ch._numUnread = unr
+        FileReader.updateChat(ch)
+
+    case "remove":
+        con = Contact('bob1235')
+        con2 = Contact('jerrythesnail')
+        con._displayname = 'bob'
+        con._isBlocked = False
+        con2._isBlocked = False
+        con._status = ContactStatus(2)
+        con2._status = ContactStatus(1)
+        ch = Chat([con, con2])
+        FileReader.removeChat(ch)
+
+    case "get":
+        con = Contact('bob1235')
+        con2 = Contact('jerrythesnail')
+        con._displayname = 'bob'
+        con._isBlocked = False
+        con2._isBlocked = False
+        con._status = ContactStatus(2)
+        con2._status = ContactStatus(1)
+        ch = Chat([con, con2])
+        print(FileReader.getChat(ch).toJsonObj())
--- src/pytuichat/filereader.py
+++ src/pytuichat/filereader.py
@@ -2,178 +2,179 @@ import os
 import json
 from chat import *
 
-# Makes the necessary folder for the settings and adds the settings to it
-def makeSettings():
-    defaults = {'show_nicknames' : 'yes',
-                'highlight_color' : 'yellow',
-                'sort_by' : 'most_recent_message',
-                'confirm_deletion' : 'yes'
-                }
-    
-    title = "settings.json"
-    _home = os.path.expanduser("~")
-    dir_path = os.environ.get("XDG_CONFIG_HOME") or \
-            os.path.join(_home, ".config","pytui")
-    full_path = os.path.join(dir_path, title)
-    os.makedirs(os.path.dirname(full_path), exist_ok = True)
-    try:
+class FileReader:
+    # Makes the necessary folder for the settings and adds the settings to it
+    def makeSettings():
+        defaults = {'show_nicknames' : 'yes',
+                    'highlight_color' : 'yellow',
+                    'sort_by' : 'most_recent_message',
+                    'confirm_deletion' : 'yes'
+                    }
+        
+        title = "settings.json"
+        _home = os.path.expanduser("~")
+        dir_path = os.environ.get("XDG_CONFIG_HOME") or \
+                os.path.join(_home, ".config","pytui")
+        full_path = os.path.join(dir_path, title)
+        os.makedirs(os.path.dirname(full_path), exist_ok = True)
+        try:
+            with open(full_path, 'w') as f:
+                json.dump(defaults, f, indent=4)
+        except FileExistsError:
+            print(full_path, "already exists")
+        except Exception as e:
+            print('Error:\n', e)
+
+    # Returns a string representing the current value of setting in the settings file
+    # Returns None if an error occurs
+    def getSetting(setting):
+        title = "settings.json"
+        _home = os.path.expanduser("~")
+        dir_path = os.environ.get("XDG_CONFIG_HOME") or \
+                os.path.join(_home, ".config","pytui")
+        full_path = os.path.join(dir_path, title)
+        try:
+            with open(full_path, "r") as f:
+                settings = json.load(f)
+                output = settings[setting]
+                f.close()
+                return output
+        except FileNotFoundError:
+            print(full_path, "was not found")
+            return None
+        except Exception as e:
+            print("Error:\n", e, sep="")
+            return None
+
+    # Changes the value of setting in the settings file to new
+    # Returns True if the update was successful or False otherwise
+    def changeSetting(setting, new):
+        title = "settings.json"
+        _home = os.path.expanduser("~")
+        dir_path = os.environ.get("XDG_CONFIG_HOME") or \
+                os.path.join(_home, ".config","pytui")
+        full_path = os.path.join(dir_path, title)
+        try:
+            with open(full_path, "r") as f:
+                settings = json.load(f)
+                settings[setting] = new
+                f.close()
+                json.dump(settings, open(full_path, "w"), indent=4)
+                return True
+        except FileNotFoundError:
+            print(full_path, "was not found")
+            return False
+        except Exception as e:
+            print("Error:\n", e, sep="")
+            return False
+
+    # Create a new conversation in the pytui local data folder or update an existing one
+    # The new conversation will be based on the given Chat object
+    def updateChat(chat):
+        # Might be best to come up with a more concise title convention
+        # This will do for now, though
+        title = ""
+        for user in chat._participants:
+            title += user.getUsername()
+        title += ".json"
+        _home = os.path.expanduser("~")
+        dir_path = os.environ.get("XDG_DATA_HOME") or \
+                os.path.join(_home, '.local', 'share', "pytui")
+        full_path = os.path.join(dir_path, title)
+        os.makedirs(os.path.dirname(full_path), exist_ok = True)
         with open(full_path, 'w') as f:
-            json.dump(defaults, f, indent=4)
-    except FileExistsError:
-        print(full_path, "already exists")
-    except Exception as e:
-        print('Error:\n', e)
+            json.dump(chat.toJsonObj(), f, indent=4)
 
-# Returns a string representing the current value of setting in the settings file
-# Returns None if an error occurs
-def getSetting(setting):
-    title = "settings.json"
-    _home = os.path.expanduser("~")
-    dir_path = os.environ.get("XDG_CONFIG_HOME") or \
-            os.path.join(_home, ".config","pytui")
-    full_path = os.path.join(dir_path, title)
-    try:
-        with open(full_path, "r") as f:
-            settings = json.load(f)
-            output = settings[setting]
-            f.close()
-            return output
-    except FileNotFoundError:
-        print(full_path, "was not found")
-        return None
-    except Exception as e:
-        print("Error:\n", e, sep="")
-        return None
+    # Should remove json file representing the given Chat object
+    # Will most likely be used when blocking users
+    def removeChat(chat):
+        title = ""
+        for user in chat._participants:
+            title += user.getUsername()
+        title += ".json"
+        _home = os.path.expanduser("~")
+        dir_path = os.environ.get("XDG_DATA_HOME") or \
+                os.path.join(_home, '.local', 'share', "pytui")
+        full_path = os.path.join(dir_path, title)
+        try:
+            os.remove(full_path)
+        except FileNotFoundError:
+            print(full_path, "was not found")
+        except Exception as e:
+            print("Error:\n", e, sep="")
 
-# Changes the value of setting in the settings file to new
-# Returns True if the update was successful or False otherwise
-def changeSetting(setting, new):
-    title = "settings.json"
-    _home = os.path.expanduser("~")
-    dir_path = os.environ.get("XDG_CONFIG_HOME") or \
-            os.path.join(_home, ".config","pytui")
-    full_path = os.path.join(dir_path, title)
-    try:
-        with open(full_path, "r") as f:
-            settings = json.load(f)
-            settings[setting] = new
-            f.close()
-            json.dump(settings, open(full_path, "w"), indent=4)
-            return True
-    except FileNotFoundError:
-        print(full_path, "was not found")
-        return False
-    except Exception as e:
-        print("Error:\n", e, sep="")
-        return False
+    # Returns Chat object represented by the json file with the given title
+    # Returns None if the file is not found
+    def getChat(title):
+        _home = os.path.expanduser("~")
+        dir_path = os.environ.get("XDG_DATA_HOME") or \
+                os.path.join(_home, '.local', 'share', "pytui")
+        full_path = os.path.join(dir_path, title)
+        try:
+            with open(full_path, "r") as f:
+                return Chat.fromJsonObj(json.load(f))
+        except FileNotFoundError:
+            return None
 
-# Create a new conversation in the pytui local data folder or update an existing one
-# The new conversation will be based on the given Chat object
-def updateChat(chat):
-    # Might be best to come up with a more concise title convention
-    # This will do for now, though
-    title = ""
-    for user in chat._participants:
-        title += user.getUsername()
-    title += ".json"
-    _home = os.path.expanduser("~")
-    dir_path = os.environ.get("XDG_DATA_HOME") or \
-            os.path.join(_home, '.local', 'share', "pytui")
-    full_path = os.path.join(dir_path, title)
-    os.makedirs(os.path.dirname(full_path), exist_ok = True)
-    with open(full_path, 'w') as f:
-        json.dump(chat.toJsonObj(), f, indent=4)
+    # Store a DelivaryMessage that has not been received to reattempt sending later
+    def storeMessage(dmessage):
+        title = ".unsent.json"
+        _home = os.path.expanduser("~")
+        dir_path = os.environ.get("XDG_DATA_HOME") or \
+                os.path.join(_home, '.local', 'share', "pytui")
+        full_path = os.path.join(dir_path, title)
+        if os.path.isfile(full_path):
+            # Add the DeliveryMessage to the .unsent file
+            with open(full_path, "r") as f:
+                unsentList = json.load(f)
+                unsentList.append(dmessage.toJsonObj())
+                f.close()
+                json.dump(unsentList, open(full_path, "w"), indent=4)
+        else:
+            # Create a new .unsent file with a list containing the DeliveryMessage
+            json.dump([dmessage.toJsonObj()], open(full_path, "w"), indent=4)
 
-# Should remove json file representing the given Chat object
-# Will most likely be used when blocking users
-def removeChat(chat):
-    title = ""
-    for user in chat._participants:
-        title += user.getUsername()
-    title += ".json"
-    _home = os.path.expanduser("~")
-    dir_path = os.environ.get("XDG_DATA_HOME") or \
-            os.path.join(_home, '.local', 'share', "pytui")
-    full_path = os.path.join(dir_path, title)
-    try:
-        os.remove(full_path)
-    except FileNotFoundError:
-        print(full_path, "was not found")
-    except Exception as e:
-        print("Error:\n", e, sep="")
+    # Removes all DeliveryMessages with empty sendingTo lists
+    def clearSent():
+        title = ".unsent.json"
+        _home = os.path.expanduser("~")
+        dir_path = os.environ.get("XDG_DATA_HOME") or \
+                os.path.join(_home, '.local', 'share', "pytui")
+        full_path = os.path.join(dir_path, title)
+        try:
+            with open(full_path, "r") as f:
+                unsentList = json.load(f)
+                targets = []
+                for i in range(0, len(unsentList)):
+                    if len(unsentList[i]["sendingTo"]) < 1:
+                        targets.append(i)
+                for t in reversed(targets):
+                    unsentList.pop(t)
+                f.close()
+                json.dump(unsentList, open(full_path, "w"), indent=4)
+        except FileNotFoundError:
+            # If there is no .unsent, no messages to clear
+            pass
+        except Exception as e:
+            print("Error:\n", e, sep="")
 
-# Returns Chat object represented by the json file with the given title
-# Returns None if the file is not found
-def getChat(title):
-    _home = os.path.expanduser("~")
-    dir_path = os.environ.get("XDG_DATA_HOME") or \
-            os.path.join(_home, '.local', 'share', "pytui")
-    full_path = os.path.join(dir_path, title)
-    try:
-        with open(full_path, "r") as f:
-            return Chat.fromJsonObj(json.load(f))
-    except FileNotFoundError:
-        return None
-
-# Store a DelivaryMessage that has not been received to reattempt sending later
-def storeMessage(dmessage):
-    title = ".unsent.json"
-    _home = os.path.expanduser("~")
-    dir_path = os.environ.get("XDG_DATA_HOME") or \
-            os.path.join(_home, '.local', 'share', "pytui")
-    full_path = os.path.join(dir_path, title)
-    if os.path.isfile(full_path):
-        # Add the DeliveryMessage to the .unsent file
-        with open(full_path, "r") as f:
-            unsentList = json.load(f)
-            unsentList.append(dmessage.toJsonObj())
-            f.close()
-            json.dump(unsentList, open(full_path, "w"), indent=4)
-    else:
-        # Create a new .unsent file with a list containing the DeliveryMessage
-        json.dump([dmessage.toJsonObj()], open(full_path, "w"), indent=4)
-
-# Removes all DeliveryMessages with empty sendingTo lists
-def clearSent():
-    title = ".unsent.json"
-    _home = os.path.expanduser("~")
-    dir_path = os.environ.get("XDG_DATA_HOME") or \
-            os.path.join(_home, '.local', 'share', "pytui")
-    full_path = os.path.join(dir_path, title)
-    try:
-        with open(full_path, "r") as f:
-            unsentList = json.load(f)
-            targets = []
-            for i in range(0, len(unsentList)):
-                if len(unsentList[i]["sendingTo"]) < 1:
-                    targets.append(i)
-            for t in reversed(targets):
-                unsentList.pop(t)
-            f.close()
-            json.dump(unsentList, open(full_path, "w"), indent=4)
-    except FileNotFoundError:
-        # If there is no .unsent, no messages to clear
-        pass
-    except Exception as e:
-        print("Error:\n", e, sep="")
-
-# Returns a list containing all DeliveryMessages in .unsent.json
-# Will include those that have empty sendingTo lists
-# Call clearSent() first or filter list if undesirable
-def getUnsent():
-    title = ".unsent.json"
-    _home = os.path.expanduser("~")
-    dir_path = os.environ.get("XDG_DATA_HOME") or \
-            os.path.join(_home, '.local', 'share', "pytui")
-    full_path = os.path.join(dir_path, title)
-    try:
-        with open(full_path, "r") as f:
-            dms = []
-            unsentList = json.load(f)
-            for dm in unsentList:
-                dms.append(DeliveryMessage.fromJsonObj(dm))
-            return dms
-    except FileNotFoundError:
-        print(full_path, "was not found.")
-    except Exception as e:
-        print("Error:\n", e, sep="")
+    # Returns a list containing all DeliveryMessages in .unsent.json
+    # Will include those that have empty sendingTo lists
+    # Call clearSent() first or filter list if undesirable
+    def getUnsent():
+        title = ".unsent.json"
+        _home = os.path.expanduser("~")
+        dir_path = os.environ.get("XDG_DATA_HOME") or \
+                os.path.join(_home, '.local', 'share', "pytui")
+        full_path = os.path.join(dir_path, title)
+        try:
+            with open(full_path, "r") as f:
+                dms = []
+                unsentList = json.load(f)
+                for dm in unsentList:
+                    dms.append(DeliveryMessage.fromJsonObj(dm))
+                return dms
+        except FileNotFoundError:
+            print(full_path, "was not found.")
+        except Exception as e:
+            print("Error:\n", e, sep="")
